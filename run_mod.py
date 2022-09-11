dd6e38c430c254368835fab5eba531846058d94d
Darkpuzzy <zhirkov_lol@mail.ru>
2022-09-11 23:22:39 +0300

    added terminal interface

---
+++ run_mod.py
@@ -0,0 +1,33 @@
+''' Import line '''
+
+import asyncio
+import os
+import sys
+import datetime
+import time
+
+''' PARSE FUNCTION '''
+
+
+async def parse_message(message: str):
+    pars_mode = message.replace("\n","")
+    return pars_mode
+
+''' STARTED LOCAL PROGRAM '''
+
+if __name__ == "__main__":
+    stop_word = True
+    print('Hello, this microservice is for tracking crypto transaction and wallet ')
+    print('The first step is registration your wallet and user info\nEnter /reg and /help for more info')
+
+    while stop_word:
+        for line in sys.stdin:
+            message = asyncio.run(parse_message(line))
+            if message == '/help':
+                print('\n| INFO FOR COMANDLINE |\n\n -/help - more info\n -/stop - stopped process')
+            if message == '/reg':
+                pass
+            if message == '/stop':
+                print('\nStoped process successfully!')
+                stop_word = False
+                break
