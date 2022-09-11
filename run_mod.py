2f034bc520e17b6171724b4d551de30882c32d7b
Darkpuzzy <zhirkov_lol@mail.ru>
2022-09-12 01:37:26 +0300

    added commands

--- run_mod.py
+++ run_mod.py
@@ -5,7 +5,7 @@ import os
 import sys
 import datetime
 import time
-
+from model.connect import migrate
 ''' PARSE FUNCTION '''
 
 
@@ -17,17 +17,33 @@ async def parse_message(message: str):
 
 if __name__ == "__main__":
     stop_word = True
-    print('Hello, this microservice is for tracking crypto transaction and wallet ')
+    print('\nHello, this microservice is for tracking crypto transaction and wallet ')
     print('The first step is registration your wallet and user info\nEnter /reg and /help for more info\n')
 
     while stop_word:
         for line in sys.stdin:
             message = asyncio.run(parse_message(line))
             if message == '/help':
-                print('\n| INFO FOR COMANDLINE |\n\n -/help - more info\n -/stop - stopped process\n')
+                print('\n| INFO FOR COMANDLINE |\n\n -/help - more info\n -/stop - stopped process\n -/run_db - init database\n')
             if message == '/reg':
                 pass
+            if message == '/get_info':
+                stop_word2  = True
+                while stop_word2:
+                    for line2 in sys.stdin:
+                        message = asyncio.run(parse_message(line2))
+                        if message == '/get_txs':
+                            print('YOUR LAST txs is ....')
+                        if message == '/quit':
+                            stop_word2 = False
+                            print('You in menu')
+                            break
+
+            if message == '/run_db':
+                print(migrate())
+                print('Init databases')
             if message == '/stop':
                 print('\nStoped process successfully!')
                 stop_word = False
                 break
+
