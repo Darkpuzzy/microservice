f494c20be9585d7fde2e12060fef11dce199e25b
Darkpuzzy <zhirkov_lol@mail.ru>
2022-09-12 15:21:14 +0300

    added info

--- run_mod.py
+++ run_mod.py
@@ -10,10 +10,11 @@ from model.connect import migrate
 
 
 async def parse_message(message: str):
-    pars_mode = message.replace("\n","")
+    pars_mode = message.replace("\n", "")
     return pars_mode
 
 ''' STARTED LOCAL PROGRAM '''
+# запуск клиента в доработке
 
 if __name__ == "__main__":
     stop_word = True
@@ -21,14 +22,14 @@ if __name__ == "__main__":
     print('The first step is registration your wallet and user info\nEnter /reg and /help for more info\n')
 
     while stop_word:
-        for line in sys.stdin:
+        for line in sys.stdin:  # sys.stdin ожидающий цикл для принятия сообщений, активится в момент поступления
             message = asyncio.run(parse_message(line))
             if message == '/help':
                 print('\n| INFO FOR COMANDLINE |\n\n -/help - more info\n -/stop - stopped process\n -/run_db - init database\n')
             if message == '/reg':
                 pass
             if message == '/get_info':
-                stop_word2  = True
+                stop_word2 = True
                 while stop_word2:
                     for line2 in sys.stdin:
                         message = asyncio.run(parse_message(line2))
